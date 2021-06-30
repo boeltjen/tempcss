e491fbce03974b5e3cfb1f56b981326f1fbfe1c8
boeltjen <16950666+boeltjen@users.noreply.github.com>
2021-06-30 14:12:34 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,53 +12,53 @@ if(firstElement.find("#breadcrumbs").length == 0) {
 $(".button, .mdc-button").addClass("btn btn-primary");
 
 
-// var appointmentDateSections = [];
-// $("div.date.one-queue").each(function() {
-// 	var tempDateTitle = $(this).find("a.title").eq(0).text().trim();
-// 	var tempDateTitleId = tempDateTitle.replace(/[^a-zA-Z0-9]/g, '');
-// 	var tempTimesLinks = $(this).find("ul.times-list a");
-// 	/*.each(function() {
-// 		tempTimesLinks.push($("<a/>").html($(this).html().replace(/\s/g, '')));
-// 	});
-// 	*/
-// 	var tempDateHeaderEle = $("<h2/>")
-// 		.attr({
-// 		  "data-type":"toggle",
-// 		  "aria-controls":tempDateTitleId,
-// 		  "aria-expanded":"false"
-// 		})
-// 		.text(tempDateTitle);
-// 	var tempInnerDiv = $("<div/>")
-// 		.attr({
-// 		  "id":tempDateTitleId,
-// 		});
-// 	tempInnerDiv.append(tempTimesLinks);
+var appointmentDateSections = [];
+$("div.date.one-queue").eq(0).each(function() {
+	var tempDateTitle = $(this).find("a.title").eq(0).text().trim();
+	var tempDateTitleId = tempDateTitle.replace(/[^a-zA-Z0-9]/g, '');
+	var tempTimesLinks = $(this).find("ul.times-list a");
+	/*.each(function() {
+		tempTimesLinks.push($("<a/>").html($(this).html().replace(/\s/g, '')));
+	});
+	*/
+	var tempDateHeaderEle = $("<h2/>")
+		.attr({
+		  "data-type":"toggle",
+		  "aria-controls":tempDateTitleId,
+		  "aria-expanded":"false"
+		})
+		.text(tempDateTitle);
+	var tempInnerDiv = $("<div/>")
+		.attr({
+		  "id":tempDateTitleId,
+		});
+	tempInnerDiv.append(tempTimesLinks);
 	
-// 	var tempSectionEle = $("<section/>").append(tempDateHeaderEle).append(tempInnerDiv);
+	var tempSectionEle = $("<section/>").append(tempDateHeaderEle).append(tempInnerDiv);
 
 
-// 	/*
-// 	appointmentDates.push({
-// 		dateTitleId: tempDateTitleId,
-// 		dateTitle: tempDateTitle,
-// 		timesLinks: tempTimesLinks,
-// 		sectionEle: tempSectionEle
-// 	});
-// 	*/
-// 	appointmentDateSections.push(tempSectionEle)
-// });
-// var accordionEle = $("<cotui-accordion/>")
-// 	.attr({
-// 	  "id": "times-list-accordion",
-// 	  "class": "cot-accordion",
-// 	  "data-title": "Select Appointment Time",
-// 	  "data-level": "2",
-// 	  "data-button-expand": "btn btn-link",
-// 	  "data-button-collapse": "btn btn-link",
-// 	  "data-allow-multiple": true
-// 	})
-// 	.append(appointmentDateSections);
+	/*
+	appointmentDates.push({
+		dateTitleId: tempDateTitleId,
+		dateTitle: tempDateTitle,
+		timesLinks: tempTimesLinks,
+		sectionEle: tempSectionEle
+	});
+	*/
+	appointmentDateSections.push(tempSectionEle)
+});
+var accordionEle = $("<cotui-accordion/>")
+	.attr({
+	  "id": "times-list-accordion",
+	  "class": "cot-accordion",
+	  "data-title": "Select Appointment Time",
+	  "data-level": "2",
+	  "data-button-expand": "btn btn-link",
+	  "data-button-collapse": "btn btn-link",
+	  "data-allow-multiple": true
+	})
+	.append(appointmentDateSections);
 
-// //$("div.date.one-queue").remove();
+//$("div.date.one-queue").remove();
 
-// $("#dateTimesContainer").append(accordionEle);
+$("#dateTimesContainer").append(accordionEle);
