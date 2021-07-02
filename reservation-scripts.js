2994c559fbaecedc0f93ceb2d81b348ed3e376ff
boeltjen <16950666+boeltjen@users.noreply.github.com>
2021-07-02 12:53:40 -0400

    Update reservation-scripts.js

--- reservation-scripts.js
+++ reservation-scripts.js
@@ -3,11 +3,16 @@ $('link[rel=stylesheet][href*="bundle.css"]').remove();
 
 // if first element isn't the breadcrumb, move it to under the breadcrumb.
 // check if the first element is the breadcrumb (= 0 if not)
-var firstElement = $(".body-layout div:not([style*='display: none']").eq(0);
+/*var firstElement = $(".body-layout div:not([style*='display: none']").eq(0);
 if(firstElement.find("#breadcrumbs").length == 0) {
 	//first element isn't the breadcrumb.  move it to under the breadcrumb.
 	firstElement.insertAfter($("#breadcrumbs").parent());
-}
+}*/
+
+
+//find the breadcrumbs and place it under the header
+var breadcrumbElement = $("#breadcrumbs").parent();
+breadcrumbElement.insertAfter($("header"));
 
 $(".button, .mdc-button").addClass("btn btn-primary");
 
