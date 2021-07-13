263da0686c7d3aff58862258d895a875602bab66
boeltjen <16950666+boeltjen@users.noreply.github.com>
2021-07-13 12:24:21 -0400

    Update reservation-scripts.js

--- reservation-scripts.js
+++ reservation-scripts.js
@@ -74,14 +74,16 @@ var cframeFooterHtml = `
 	    <div w3-include-html="https://cors-anywhere.herokuapp.com/https://www.toronto.ca/globalnav/iwantto/"><a
 			href="https://cors-anywhere.herokuapp.com/https://www.toronto.ca/globalnav/iwantto/"></a></div>
 	</div>
-	<script src="https://www.toronto.ca/wp-content/themes/cot/js/jquery-3.3.1.min.js"></script>
-	<script src="https://www.toronto.ca/wp-content/themes/cot/js/jquery-migrate-3.0.1.min.js"></script>
-	<script src="https://www.toronto.ca/cot/jquery-additional-custom-js.js?ver=1.0.0"></script>
 
-
-	<script src="https://www.toronto.ca/wp-content/themes/cot/js/bootstrap.3.4.1.min.js"></script>
-
-	<script src="https://www.toronto.ca/wp-content/themes/cot/js/footer.js"></script>
+	<script>
+		//activate the w3-includes:
+		w3IncludeHTML(function () {
+			var scriptTag = document.createElement('script'), // create a script tag
+			firstScriptTag = document.getElementsByTagName('script')[0]; // find the first script tag in the document
+			scriptTag.src = "https://www.toronto.ca/wp-content/themes/cot/js/scripts.js";  // set the source of the script to your script
+			firstScriptTag.parentNode.insertBefore(scriptTag, firstScriptTag);
+		});
+	</script>
 `;
 
 //remove stock stylesheets
@@ -104,15 +106,6 @@ if(firstElement.find("#breadcrumbs").length == 0) {
 $("footer").eq(0).replaceWith(cframeFooterHtml);
 $("header").eq(0).replaceWith(cframeHeaderHtml);
 
-$( document ).ready(function() {
-	//activate the w3-includes:
-	w3IncludeHTML(function () {
-		var scriptTag = document.createElement('script'), // create a script tag
-		firstScriptTag = document.getElementsByTagName('script')[0]; // find the first script tag in the document
-		scriptTag.src = "https://www.toronto.ca/wp-content/themes/cot/js/scripts.js";  // set the source of the script to your script
-		firstScriptTag.parentNode.insertBefore(scriptTag, firstScriptTag);
-	});
-});
 
 
 //find the breadcrumbs and place it under the header
