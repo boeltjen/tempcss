3f72c4dda73d088d59ac1e4a6010d6597cf42bd1
boeltjen <16950666+boeltjen@users.noreply.github.com>
2021-08-12 01:42:59 -0400

    Update reservation-scripts.js

--- reservation-scripts.js
+++ reservation-scripts.js
@@ -375,7 +375,7 @@ divs_w_shortcodes.each(function() {
 	}
 	
 	//add any added roles
-	if(rolesToAdd) $(this).attr("role",$(this).attr("role") + " " + rolesToAdd);
+	if(rolesToAdd) $(this).attr("role",($(this).attr("role") || "") + " " + rolesToAdd.trim());
 
 	
 	//add/update any added aria
