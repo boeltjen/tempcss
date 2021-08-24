44490b1713cc3a80ccc440e7632f2b6d6d4ab765
boeltjen <16950666+boeltjen@users.noreply.github.com>
2021-08-24 18:05:40 -0400

    Update reservation-scripts.js

--- reservation-scripts.js
+++ reservation-scripts.js
@@ -444,7 +444,7 @@ if(datelistElement.length > 0) {
 	$("body").css("display","block");
 	
 	//refresh alert elements since not ajax to trigger sr-alerts
-	refreshAlertElements(100);
+	refreshAlertElements(500);
 
 	//convert date-time selector to cotui
 	divDateOneQueues.each(function() {
@@ -544,7 +544,7 @@ if(datelistElement.length > 0) {
 	// $("#torontopageheader").focus();
 	
 	//refresh alert elements since not ajax to trigger sr-alerts
-	refreshAlertElements(100);
+	refreshAlertElements(500);
 		
 }
 
