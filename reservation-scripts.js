677bb7136c6758db7bcfda988e8ffc4ea50e0bf4
boeltjen <16950666+boeltjen@users.noreply.github.com>
2021-09-07 00:44:05 -0400

    Update reservation-scripts.js

--- reservation-scripts.js
+++ reservation-scripts.js
@@ -1,3 +1,6 @@
+var defaultContactInfoHtmlString =  
+    '<p class="contact-information"><strong>Toronto City Hall</strong><br>100 Queen Street West<br>(main entrance off Nathan Phillips Square)<br><strong>Telephone</strong>: 416-392-7036</p><p class="contact-information"><strong>Email</strong>: marriage@toronto.ca</p>';
+
 if(defaultContactInfoHtmlString == undefined) {
 	var defaultContactInfoHtmlString = 
 	'				            <p class="contact-information">'
