86455aa360e3182f2edec832f7d95335371f49ea
boeltjen <16950666+boeltjen@users.noreply.github.com>
2021-09-07 00:57:21 -0400

    Update contactinfo.js

--- contactinfo.js
+++ contactinfo.js
@@ -1,9 +1,10 @@
 var defaultContactInfoHtmlString = 
-'<p class="contact-information">'
-+'	<strong>Toronto City Hall</strong>'
-+'	<br>100 Queen Street West<br>'
-+'	(main entrance off Nathan Phillips Square)<br>'
-+'	<strong>Telephone</strong>: 416-392-7036</p>'
+'   <p class="contact-information">'
++'	  <strong>Toronto City Hall</strong>'
++'	  <br>100 Queen Street West<br>'
++'	  (main entrance off Nathan Phillips Square)<br>'
++'	  <strong>Telephone</strong>: 416-392-7036</p>'
 +'	<p class="contact-information">'
-+'	<strong>Email</strong>: marriage@toronto.ca</p>'
++'	  <strong>Email</strong>: marriage@toronto.ca'
++'  </p>'
 ;
